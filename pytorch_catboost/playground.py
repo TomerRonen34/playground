e54e375812d3195515b7cfda7178b51f9f1169e3
Tomer Ronen <tomer.ronen34@gmail.com>
2021-02-05 23:36:33 +0200

    playing with Catboost models

---
+++ pytorch_catboost/playground.py
@@ -0,0 +1,63 @@
+from catboost import CatBoostClassifier, CatBoostRegressor
+from sklearn.datasets import load_breast_cancer
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
+from torch.nn import BCEWithLogitsLoss
+import matplotlib.pyplot as plt
+import numpy as np
+
+from pytorch_catboost.pytorch_second_derivative import LoglossObjective, CustomPytorchObjective, minus_bce_loss, \
+    basic_dml_loss, margin_dml_loss
+
+X, y = load_breast_cancer(return_X_y=True)
+X_train, X_test, y_train, y_test = train_test_split(X, y)
+
+# model = CatBoostClassifier(loss_function=LoglossObjective(), eval_metric="Accuracy", verbose=True)
+# model = CatBoostClassifier(loss_function=CustomPytorchObjective(loss_function=minus_bce_loss), eval_metric="Accuracy", verbose=True)
+# model = CatBoostClassifier(loss_function=CustomPytorchObjective(loss_function=BCEWithLogitsLoss(reduction="sum")), eval_metric="Accuracy", verbose=True)
+
+# model = CatBoostRegressor(loss_function=CustomPytorchObjective(loss_function=basic_dml_loss),
+#                           n_estimators=1000,
+#                           eval_metric="Accuracy",
+#                           verbose=True)
+
+model = CatBoostRegressor(loss_function=CustomPytorchObjective(loss_function=margin_dml_loss),
+                          n_estimators=200,
+                          eval_metric="Accuracy",
+                          verbose=True)
+
+model.fit(X_train, y_train)
+
+pred_train = model.predict(X_train)
+
+pred0 = pred_train[y_train == 0] ** 2
+pred0 = pred0[(pred0 > np.quantile(pred0, 0.1)) & (pred0 < np.quantile(pred0, 0.9))]
+plt.hist(pred0, label="0", bins=10, alpha=0.5)
+plt.title("train 0")
+plt.show()
+
+pred1 = pred_train[y_train == 1] ** 2
+pred1 = pred1[(pred1 > np.quantile(pred1, 0.1)) & (pred1 < np.quantile(pred1, 0.9))]
+plt.hist(pred1, label="1", bins=10, alpha=0.5)
+plt.title("train 1")
+plt.show()
+
+pred_test = model.predict(X_test)
+
+pred0 = pred_test[y_test == 0] ** 2
+pred0 = pred0[(pred0 > np.quantile(pred0, 0.1)) & (pred0 < np.quantile(pred0, 0.9))]
+plt.hist(pred0, label="0", bins=10, alpha=0.5)
+plt.title("test 0")
+plt.show()
+
+pred1 = pred_test[y_test == 1] ** 2
+pred1 = pred1[(pred1 > np.quantile(pred1, 0.1)) & (pred1 < np.quantile(pred1, 0.9))]
+plt.hist(pred1, label="1", bins=10, alpha=0.5)
+plt.title("test 1")
+plt.legend()
+plt.show()
+
+print(classification_report(y_train, pred_train ** 2 < 50))
+print(classification_report(y_test, pred_test ** 2 < 50))
+
+a = 0
--- pytorch_catboost/pytorch_second_derivative.py
+++ pytorch_catboost/pytorch_second_derivative.py
@@ -3,10 +3,11 @@ from typing import Callable, Tuple, Sequence, List
 import numpy as np
 import torch
 from torch import Tensor
+from torch.nn import BCEWithLogitsLoss, MSELoss
 
 
 class LoglossObjective:
-    def calc_ders_range(self, approxes, targets, weights):
+    def calc_ders_range(self, approxes, targets, weights=None):
         assert len(approxes) == len(targets)
         if weights is not None:
             assert len(weights) == len(approxes)
@@ -78,8 +79,8 @@ class CustomPytorchObjective:
         approxes = torch.FloatTensor(approxes).requires_grad_()
         targets = torch.FloatTensor(targets)
 
-        loss = self.loss_function(approxes, targets)
-        der1, der2 = self._calculate_derivatives(loss, approxes)
+        objective = - self.loss_function(approxes, targets)
+        der1, der2 = self._calculate_derivatives(objective, approxes)
 
         if weights is not None:
             weights = np.asarray(weights)
@@ -89,8 +90,8 @@ class CustomPytorchObjective:
         return result
 
     @staticmethod
-    def _calculate_derivatives(loss: Tensor, approxes: Tensor) -> Tuple[np.ndarray, np.ndarray]:
-        der1, = torch.autograd.grad(loss, approxes, create_graph=True)
+    def _calculate_derivatives(objective: Tensor, approxes: Tensor) -> Tuple[np.ndarray, np.ndarray]:
+        der1, = torch.autograd.grad(objective, approxes, create_graph=True)
 
         der2 = []
         for i in range(len(approxes)):
@@ -115,29 +116,106 @@ def logloss(approxes: Tensor, targets: Tensor) -> Tensor:
     return loss
 
 
-def compare_methods():
+def minus_bce_loss(approxes, targets):
+    return - BCEWithLogitsLoss(reduction="sum")(approxes, targets)
+
+
+def compare_methods_logloss():
     # TODO: check minus in loss func. Why doesn't the Patbas do it?
     n = 100
     approxes = np.random.randn(n)
     targets = np.random.randint(low=0, high=2, size=n)
+    # weights = np.random.rand(n)
     weights = None
 
     result_patbas = LoglossObjective().calc_ders_range(approxes, targets, weights)
     result_pytorch_explicit = LoglossPytorchExplicit().calc_ders_range(approxes, targets, weights)
     result_pytorch_autograd = LoglossPytorchAutograd().calc_ders_range(approxes, targets, weights)
     result_custom_pytorch = CustomPytorchObjective(loss_function=logloss).calc_ders_range(approxes, targets, weights)
+    result_custom_pytorch_builtin = CustomPytorchObjective(loss_function=BCEWithLogitsLoss(reduction="sum")
+                                                           ).calc_ders_range(approxes, targets, weights)
+    result_custom_pytorch_minus_builtin = CustomPytorchObjective(loss_function=minus_bce_loss
+                                                                 ).calc_ders_range(approxes, targets, weights)
 
     print(result_patbas)
     print(result_pytorch_explicit)
     print(result_pytorch_autograd)
     print(result_custom_pytorch)
+    print(result_custom_pytorch_builtin)
+    print(result_custom_pytorch_minus_builtin)
 
     print(np.allclose(result_patbas, result_pytorch_explicit))
     print(np.allclose(result_patbas, result_pytorch_autograd, rtol=1e-4))
     print(np.allclose(result_patbas, result_custom_pytorch, rtol=1e-4))
+    print(np.allclose(result_patbas, result_custom_pytorch_builtin, rtol=1e-4))
+    print(np.allclose(result_patbas, result_custom_pytorch_minus_builtin, rtol=1e-4))
+
+    breakhere = 1
+
+
+class RmseObjective(object):
+    def calc_ders_range(self, approxes, targets, weights):
+        assert len(approxes) == len(targets)
+        if weights is not None:
+            assert len(weights) == len(approxes)
+
+        result = []
+        for index in range(len(targets)):
+            der1 = targets[index] - approxes[index]
+            der2 = -1
+
+            if weights is not None:
+                der1 *= weights[index]
+                der2 *= weights[index]
+
+            result.append((der1, der2))
+        return result
+
+
+def rmse_loss(approxes, targets):
+    # return torch.sqrt(MSELoss(reduction="sum")(approxes, targets))
+    return 0.5 * MSELoss(reduction="sum")(approxes, targets)
+
+
+def basic_dml_loss(approxes: Tensor, targets: Tensor) -> Tensor:
+    minimize_or_maximize = (-1) ** (1 - targets)
+    # loss = minimize_or_maximize * ((approxes + 1e-4) ** 2)
+    # loss = minimize_or_maximize * ((approxes - 1) ** 2)
+    loss = minimize_or_maximize * torch.abs(approxes - 1)
+    loss = loss.sum()
+    return loss
+
+
+def margin_dml_loss(approxes: Tensor, targets: Tensor, margin: float = 100) -> Tensor:
+    loss = torch.abs(approxes - 1)
+    is_different = targets == 0
+    is_smaller_than_margin = approxes[is_different] ** 2 < margin
+    loss[is_different] *= is_smaller_than_margin * -1
+    loss = loss.sum()
+    return loss
+
+
+def predict_basic_dml(preds: np.ndarray) -> np.ndarray:
+    pass
+
+
+def compare_methods_rmse():
+    n = 100
+    approxes = np.random.randn(n)
+    targets = np.random.randint(low=0, high=2, size=n)
+    weights = None
+
+    result_patbas = RmseObjective().calc_ders_range(approxes, targets, weights)
+    result_custom_pytorch2 = CustomPytorchObjective(loss_function=rmse_loss
+                                                    ).calc_ders_range(approxes, targets, weights)
+
+    print(result_patbas)
+    print(result_custom_pytorch2)
+
+    print(np.allclose(result_patbas, result_custom_pytorch2, rtol=1e-4))
 
     breakhere = 1
 
 
 if __name__ == '__main__':
-    compare_methods()
+    compare_methods_logloss()
